1500a8ac62878d7244957eabd3ba5a93cdc0a7c9
Sam Greenhill <samgreenhill@Sams-MBP.home>
2017-05-16 21:32:57 -0400

    working to displaying the featured tasks on the summary page

--- planner/ViewController.swift
+++ planner/ViewController.swift
@@ -7,15 +7,68 @@
 //
 
 import UIKit
+import CoreData
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
+    
+    @IBOutlet var featuredTableView: UITableView!
+    
+    
+    var featuredTasksData = [NSManagedObject]()
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        featuredTableView.delegate = self
+        featuredTableView.dataSource = self
+    }
+    
+    
+    override func viewWillAppear(_ animated: Bool) {
+        let predicate = NSPredicate(format: "featured = %@ AND taskStatus = %@", true as CVarArg, false as CVarArg)
+        
+        featuredTasksData = fetchData(entityName: "ToDo", predicate: predicate, sortDescriptor: nil)
+        featuredTableView.reloadData()
     }
 
-
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return featuredTasksData.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        
+        //configure cell
+        
+        
+        cell.textLabel?.text = featuredTasksData[indexPath.row].value(forKey: "taskDescription") as! String
+        cell.textLabel?.textColor = UIColor.red
+        
+        cell.detailTextLabel?.text = featuredTasksData[indexPath.row].value(forKey: "taskDateString") as! String
+        
+        return cell
+        
+        
+        
+        
+    }
+    
+    
+    
+    
+    
+    
+    
+    
 
 }
 
